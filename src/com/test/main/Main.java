85f96bfeb908296154fb1b48cedecd2648ef0d56
Administrator <Administrator@PC-20150704GFSV>
2016-01-06 14:24:31 +0800

    反射Demo

---
+++ src/com/test/main/Main.java
@@ -0,0 +1,13 @@
+package com.test.main;
+
+import com.test.reflection.ClassUtil;
+
+public class Main {
+	public static void main(String[] args) {
+		//ClassUtil.printFields(new Integer(0));
+		
+		//ClassUtil.printMethods(new Integer(0));
+		
+		ClassUtil.printconstructors(new Integer(0));
+	}
+}
